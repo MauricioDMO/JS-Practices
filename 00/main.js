9575e9ecf7514ad33c3b9a0d1163a5bd179c2493
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2023-06-22 21:42:06 -0600

    Mejora de codigo

--- 00/main.js
+++ 00/main.js
@@ -4,15 +4,10 @@ const res = document.getElementById('res')
 const ul = document.createElement('ol')
 
 for (let i = 1; i <= 100; i++) {
-  if ((i % 3 == 0) && (i % 5 == 0)) {
-    ul.innerHTML += `<li>fizzbuzz</li>`
-  } else if (i % 3 == 0) {
-    ul.innerHTML += `<li>fizz</li>`
-  } else if (i % 5 == 0) {
-    ul.innerHTML += `<li>buzz</li>`
-  } else {
-    ul.innerHTML += `<li>${i}</li>`
-  }
+  if ((i % 3 == 0) && (i % 5 == 0)) ul.innerHTML += `<li>fizzbuzz</li>`
+  else if (i % 3 == 0) ul.innerHTML += `<li>fizz</li>`
+  else if (i % 5 == 0) ul.innerHTML += `<li>buzz</li>`
+  else ul.innerHTML += `<li>${i}</li>`
 }
 
 res.appendChild(ul)
