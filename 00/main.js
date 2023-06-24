7321d0caa915efa4bb2977351cbbac5a0a61f98f
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2023-06-24 15:00:07 -0600

    Corrección de nombre de variable

--- 00/main.js
+++ 00/main.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const res = document.getElementById('res')
-const ul = document.createElement('ol')
+const ol = document.createElement('ol')
 
 for (let i = 1; i <= 100; i++) {
-  if ((i % 3 == 0) && (i % 5 == 0)) ul.innerHTML += `<li>fizzbuzz</li>`
-  else if (i % 3 == 0) ul.innerHTML += `<li>fizz</li>`
-  else if (i % 5 == 0) ul.innerHTML += `<li>buzz</li>`
-  else ul.innerHTML += `<li>${i}</li>`
+  if ((i % 3 == 0) && (i % 5 == 0)) ol.innerHTML += `<li>fizzbuzz</li>`
+  else if (i % 3 == 0) ol.innerHTML += `<li>fizz</li>`
+  else if (i % 5 == 0) ol.innerHTML += `<li>buzz</li>`
+  else ol.innerHTML += `<li>${i}</li>`
 }
 
-res.appendChild(ul)
+res.appendChild(ol)
